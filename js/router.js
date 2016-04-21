c5913aa416cad75cf2f366ae7caaf86db4840256
Jason Sun <j_sun@apple.com>
2016-04-21 15:27:49 -0700

    display model

--- js/router.js
+++ js/router.js
@@ -2,3 +2,10 @@ Todos.Router.map(function() {
 	// tell emberjs to detect when url is '/' and render the todos template
 	this.resource('todos', { path: '/' });
 });
+
+Todos.TodosRoute = Ember.Route.extend({
+	// a TodosRoute class with a model function that returns all the existing todos
+	model: function() {
+		return this.store.find('todo');
+	}
+});
