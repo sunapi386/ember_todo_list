7b55347f65e13d1c1878c52fcb554b85e50a1ea0
Jason Sun <j_sun@apple.com>
2016-04-21 15:12:31 -0700

    fixtures

--- js/application.js
+++ js/application.js
@@ -1,2 +1,7 @@
 // create instance of Ember Application
 window.Todos = Ember.Application.create();
+
+Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+// First, update js/application.js to indicate that your application's ApplicationAdapter is an extension of the
+// DS.FixtureAdapter. Adapters are responsible for communicating with a source of data for your application. Typically
+// this will be a web service API, but in this case we are using an adapter designed to load fixture data:
--- js/models/todo.js
+++ js/models/todo.js
@@ -4,3 +4,9 @@ Todos.Todo = DS.Model.extend({
 });
 // This code creates a new class Todo and places it within your application's namespace.
 // Each todo will have two attributes: title and isCompleted.
+
+Todos.Todo.FIXTURES = [
+	{id: 1, isCompleted: true, title: 'Learn Ember.js'},
+	{id: 2, isCompleted: false, title: '...'},
+	{id: 3, isCompleted: false, title: '$$$!'},
+];
