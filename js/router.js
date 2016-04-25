a39121911186abf97f74423628cbf01bdefb6661
Jason Sun <j_sun@apple.com>
2016-04-25 12:49:46 -0700

    Show only completed todos

--- js/router.js
+++ js/router.js
@@ -3,6 +3,7 @@ Todos.Router.map(function() {
 	this.resource('todos', { path: '/' }, function () {
 		// additional child routes will go here later
 		this.route('active');
+		this.route('completed');
 	});
 });
 
@@ -23,6 +24,17 @@ Todos.TodosActiveRoute = Ember.Route.extend({
 	model: function () {
 		return this.store.filter('todo', function (todo) {
 			return !todo.get('isCompleted');
+		});
+	},
+	renderTemplate: function (controller) {
+		this.render('todos/index', {controller: controller});
+	}
+})
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+	model: function () {
+		return this.store.filter('todo', function (todo) {
+			return todo.get('isCompleted');
 		});
 	},
 	renderTemplate: function (controller) {
